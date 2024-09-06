09dd3a3935df73ca490c929192b8634b078c7364
utkarsh <utkarshtyagi959@gmail.com>
2024-09-06 23:29:12 +0530

    solving reloading error

--- src/services/request.js
+++ src/services/request.js
@@ -1,46 +1,46 @@
-import axios from 'axios';
-export const BASE_URL = window.location.origin;
-//export const BASE_URL = "https://store-sell.extensions.jiox0.de/";
+// import axios from 'axios';
+// export const BASE_URL = window.location.origin;
+// //export const BASE_URL = "https://store-sell.extensions.jiox0.de/";
 
-const client = axios.create({
-  baseURL: BASE_URL,
-  responseType: 'text/plain',
-  withCredentials: false,
-  timeout: 50000,
-  headers: {}
-});
+// const client = axios.create({
+//   baseURL: BASE_URL,
+//   responseType: 'text/plain',
+//   withCredentials: false,
+//   timeout: 50000,
+//   headers: {}
+// });
 
-const request = function ({ method, url, headers = {}, data = {}, responseType = 'text/plain' }) {
-  const onSuccess = function (response) {
-    if (response.status === 200) {
-      return response;
-    }
+// const request = function ({ method, url, headers = {}, data = {}, responseType = 'text/plain' }) {
+//   const onSuccess = function (response) {
+//     if (response.status === 200) {
+//       return response;
+//     }
 
-    return response;
-  };
+//     return response;
+//   };
 
-  const onError = function (error) {
-    let message = 'Something Went Wrong';
-    let errorObject = error?.response?.data ? JSON.parse(error?.response?.data) : null;
+//   const onError = function (error) {
+//     let message = 'Something Went Wrong';
+//     let errorObject = error?.response?.data ? JSON.parse(error?.response?.data) : null;
 
-    if (errorObject && errorObject.data && errorObject.data.errorMessage) {
-      message = errorObject.data.errorMessage;
-    }
+//     if (errorObject && errorObject.data && errorObject.data.errorMessage) {
+//       message = errorObject.data.errorMessage;
+//     }
 
-    return Promise.reject(message);
-  };
+//     return Promise.reject(message);
+//   };
 
-  const payload = {
-    method,
-    url,
-    headers: {
-      ...headers
-    },
-    data: data,
-    responseType
-  };
-  // console.log(payload);
-  return client(payload).then(onSuccess).catch(onError);
-};
+//   const payload = {
+//     method,
+//     url,
+//     headers: {
+//       ...headers
+//     },
+//     data: data,
+//     responseType
+//   };
+//   // console.log(payload);
+//   return client(payload).then(onSuccess).catch(onError);
+// };
 
-export default request;
+// export default request;
