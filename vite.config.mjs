89f770051902fd4458caaafacab2fd8e85aa62a8
utkarsh <utkarshtyagi959@gmail.com>
2024-09-09 13:37:04 +0530

    Config changes

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,21 +1,18 @@
-// https://github.com/vitejs/vite/discussions/3448
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
 
-// ----------------------------------------------------------------------
-
 export default defineConfig({
   plugins: [react(), jsconfigPaths()],
-  // https://github.com/jpuri/react-draft-wysiwyg/issues/1317
   base: './',
   define: {
     global: 'window'
   },
   build: {
-    assetsDir: 'assets',  // Ensure assets are placed in this directory
-  },  
+    assetsDir: 'assets',
+    outDir: 'dist'  // Make sure this matches Vercel's expected directory
+  },
   resolve: {
     alias: [
       {
@@ -29,15 +26,11 @@ export default defineConfig({
     ]
   },
   server: {
-    // this ensures that the browser opens upon server start
     open: true,
-    // this sets a default port to 3000
     port: 3000
   },
   preview: {
-    // this ensures that the browser opens upon preview start
     open: true,
-    // this sets a default port to 3000
     port: 3000
   }
 });
